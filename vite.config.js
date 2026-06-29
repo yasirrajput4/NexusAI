ff5e0d31f6799e2fa6ff4ad87281b68bdae0b461
Yasir Rajput <rajputyasir2005@gmail.com>
2026-06-29 22:37:09 +0530

    chore: add code splitting for optimized build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
