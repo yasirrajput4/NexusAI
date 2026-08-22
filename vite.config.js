58d7539afe711956a60b951d6cceb5c20d7f812c
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-23 01:28:17 +0530

    fix: old eslint config file is replace by new file

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    chunkSizeWarningLimit: 700,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
 });
