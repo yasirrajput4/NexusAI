d5534c5e8b2516716c75351123591fa349378d41
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-23 01:37:14 +0530

    feat: GROQ model change because of deprecation in our old model

--- src/hooks/useGroqAPI.js
+++ src/hooks/useGroqAPI.js
@@ -27,7 +27,7 @@ export function useGroqAPI() {
           Authorization: `Bearer ${GROQ_API_KEY}`,
         },
         body: JSON.stringify({
-          model: "llama-3.3-70b-versatile",
+          model: "openai/gpt-oss-120b",
           max_tokens: 1024,
           messages: formattedMessages,
         }),
